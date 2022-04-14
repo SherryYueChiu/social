1b7f05369389ad4d6724ffeceab6a123d5268f14
Sherry <sherry@lulainno.com>
2022-04-14 15:48:46 +0800

    iOS meta

--- js/index.js
+++ js/index.js
@@ -10,4 +10,4 @@ $(function () {
 });
 
 // register service worker
-navigator.serviceWorker.register('service-worker.js', { scope: "." });
+// navigator.serviceWorker.register('service-worker.js', { scope: "." });
