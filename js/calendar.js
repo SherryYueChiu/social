e21d67276873d960d24c7685c5766f9783ce9e0f
Sherry <sherry@lulainno.com>
2022-04-24 20:21:41 +0800

    更新貼文

--- js/calendar.js
+++ js/calendar.js
@@ -1,11 +1,5 @@
 var CALENDAR = [
   {
-    title: '麗寶樂園',
-    time: '2022/04/23 ~ 2022/04/24',
-    description: '目前是計畫自己去，會玩水陸+摩天輪+卡丁車',
-    link: 'https://hackmd.io/@sherryyue/H16Tfu2Q5'
-  },
-  {
     title: '高空彈跳',
     time: '2022/05/21',
     description: '已揪一人，在桃園大溪',
