90b2bc35ad1a6895ca0e2603a81d488d29ad3d30
Sherry <sherryyue.c@protonmail.com>
2022-05-09 14:45:00 +0800

    更新貼文

--- js/calendar.js
+++ js/calendar.js
@@ -4,5 +4,10 @@ var CALENDAR = [
     time: '2022/05/14',
     description: '已揪一人，在桃園大溪',
     link: 'https://hackmd.io/@sherryyue/Bk-1tz1N9'
+  },
+  {
+    title: '泳渡日月潭',
+    time: '2022/09/??',
+    description: '已揪2人，要揪到超過五人才能報名'
   }
 ]
