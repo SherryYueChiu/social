aeff3deccd18825ab38213a142e53c4a596ed24d
Sherry <sherry@lulainno.com>
2022-05-09 22:35:42 +0800

    更新貼文

--- js/calendar.js
+++ js/calendar.js
@@ -8,6 +8,7 @@ var CALENDAR = [
   {
     title: '泳渡日月潭',
     time: '2022/09/04',
-    description: '已揪2人，要至少五人才能報名'
+    description: '已揪2人，要至少五人才能報名，有興趣的話跟我說～',
+    link: 'https://hackmd.io/@sherryyue/SyfB1L8U9'
   }
 ]
