0b7c6f2dcf5d04b3c6312e262521a68278f92c0d
Sherry <sherry@lulainno.com>
2022-05-28 01:01:15 +0800

    更新貼文

--- js/calendar.js
+++ js/calendar.js
@@ -1,14 +1,8 @@
 var CALENDAR = [
   {
-    title: '高空彈跳',
-    time: '2022/05/14',
-    description: '已揪一人，在桃園大溪',
-    link: 'https://hackmd.io/@sherryyue/Bk-1tz1N9'
-  },
-  {
     title: '泳渡日月潭',
     time: '2022/09/04',
-    description: '已揪2人，要至少五人才能報名，有興趣的話跟我說～',
+    description: '已揪5人，有興趣的話跟我說～',
     link: 'https://hackmd.io/@sherryyue/SyfB1L8U9'
   }
 ]
