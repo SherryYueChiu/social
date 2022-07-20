b46e3fb0113b96e5855597b5d7450e4ec2a0cfeb
Sherry <sherryyue.c@protonmail.com>
2022-07-20 16:08:03 +0800

    更新動態

--- js/calendar.js
+++ js/calendar.js
@@ -2,7 +2,7 @@ var CALENDAR = [
   {
     title: '泳渡日月潭',
     time: '2022/09/04',
-    description: '已揪5人，有興趣的話跟我說～',
+    description: '已揪5人，我是蠻想完成全程的，不過我不確定能不能',
     link: 'https://hackmd.io/@sherryyue/SyfB1L8U9'
   },
   {
@@ -11,5 +11,10 @@ var CALENDAR = [
     description: `有點貴，不過我好想參加，如果有朋朋也衝，那我就會下定決心了
     <tag>三天兩夜</tag><tag>自行車229公里</tag><tag>四星級溫泉飯店</tag><tag>新北宜蘭</tag>`,
     link: 'https://www.asiarainbowride.com'
+  },
+  {
+    title: '南投體驗活動',
+    time: '？？？',
+    description: '在某個六日，高空彈跳、透明吊橋和日月潭水晶車廂三選二，跑一次南投就都玩一玩～有興趣的話跟我說'
   }
 ]
